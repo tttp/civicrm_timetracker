a1f334af843472868616a8906e525601e9f02b42
xavier dutoit <git@tttp.eu>
2011-05-07 23:50:57 +0200

    sync of the issue->activity working

---
+++ civicrm_timetracker.default.settings.php
@@ -0,0 +1,10 @@
+<?php
+// define the node types to sync with civicrm
+$civicrm_timetracker['node_type_issue'] ='issue';
+$civicrm_timetracker['field_activity'] ='field_activity';
+$civicrm_timetracker['activity_type_issue'] = $civicrm_timetracker['node_type_issue'];
+
+$civicrm_timetracker['node_type_project'] ='project';
+$civicrm_timetracker['field_case'] ='field_case';
+$civicrm_timetracker['case_type_project'] = $civicrm_timetracker['node_type_project'];
+?>
