8e407a495298548f0c26fa584fe8f39327cda3fd
xavier dutoit <git@tttp.eu>
2011-05-08 20:36:24 +0200

    Changes, now works for case & activities (one way)

--- civicrm_timetracker.default.settings.php
+++ civicrm_timetracker.default.settings.php
@@ -5,6 +5,6 @@ $civicrm_timetracker['field_activity'] ='field_activity';
 $civicrm_timetracker['activity_type_issue'] = $civicrm_timetracker['node_type_issue'];
 
 $civicrm_timetracker['node_type_project'] ='project';
-$civicrm_timetracker['field_case'] ='field_case';
+$civicrm_timetracker['field_case'] ='field_project';
 $civicrm_timetracker['case_type_project'] = $civicrm_timetracker['node_type_project'];
 ?>
